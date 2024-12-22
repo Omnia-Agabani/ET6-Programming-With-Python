df27b6f29ddff37be62c5fa5ff1fed0768f0524b
Omnia-Agabani <omniaagabani99@gmail.com>
2024-12-22 14:17:16 +0400

    Changes

--- 3_documenting_and_testing/exercises/mystery_1.py
+++ 3_documenting_and_testing/exercises/mystery_1.py
@@ -1,2 +1,15 @@
+"""
+    The function add two value from the same type
+        
+    Parameters:
+    2 values from the same type
+    
+    Return:
+    The summation of the 2 value
+    
+    
+"""
+
 def mystery_1(a,b):
+    assert type(a) is type(b), "a and b are not the same type"  
     return a + b
--- 3_documenting_and_testing/exercises/tests/test_mystery_1.py
+++ 3_documenting_and_testing/exercises/tests/test_mystery_1.py
@@ -1,6 +1,28 @@
+from typing import Self
 import unittest
 
 from ..mystery_1 import mystery_1
 
 class TestMystery1(unittest.TestCase):
     """ """
+
+    def test_minimal_input(self):
+        """"""
+        self.assertEqual(mystery_1(0, 0), 0)
+    def test_2_int_input(self):
+        """"""
+        self.assertEqual(mystery_1(8, 9), 17)
+    def test_2_str_input(self):
+        """"""
+        self.assertEqual(mystery_1('s', 'a'), 'sa')
+    def test_2_lists_of_int_input(self):
+        """"""
+        self.assertEqual(mystery_1([1,5], [4,3]), [1, 5, 4, 3])
+    def test_2_lists_of_str_input(self):
+        """"""
+        self.assertEqual(mystery_1(['1','5'], ['4','3']), ['1', '5', '4', '3'])
+    def test_2_different_type_input(self):  
+        """"""
+        with self.assertRaises(AssertionError):
+            mystery_1('s', 1)  
+    
