bd069750feb3c8761a92560652d8a5bf17a5b789
Omnia-Agabani <omniaagabani99@gmail.com>
2024-12-22 14:37:23 +0400

    Solution

--- 3_documenting_and_testing/exercises/mystery_2.py
+++ 3_documenting_and_testing/exercises/mystery_2.py
@@ -1,4 +1,16 @@
+"""
+    The function is check the length of the input
+    
+    Parameter:
+    a value we want to check it's length
+    
+    Return:
+    The length of the input    
+    
+    
+    """
 def mystery_2(a):
+    assert type(a) is not int, "object of type int has no len() "
     if len(a) == 0:
         return None
 
--- 3_documenting_and_testing/exercises/tests/test_mystery_2.py
+++ 3_documenting_and_testing/exercises/tests/test_mystery_2.py
@@ -1,6 +1,30 @@
 import unittest
 
+
+
+
 from ..mystery_2 import mystery_2
 
 class TestMystery2(unittest.TestCase):
     """ """
+
+    def test_minimal_list_input(self):
+        """"""
+        self.assertEqual(mystery_2([]), None)
+
+    def test_minimal_string_input(self):
+        """"""
+        self.assertEqual(mystery_2(''), None)
+    def test_short_string_input(self):
+        """"""
+        self.assertEqual(mystery_2('Omnia Agabani'), 13)
+    def test_list_of_str_input(self):
+        """"""
+        self.assertEqual(mystery_2(['Omnia', 'Mustafa','Fawzi','Abdulgadir']), 4)
+    def test_list_of_int_input(self):
+        """"""
+        self.assertEqual(mystery_2([3,3,19,99]), 4)
+    def test_2_different_type_input(self):  
+        """"""
+        with self.assertRaises(AssertionError):
+            mystery_2(1)
