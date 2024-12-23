d1234143da43a86f687cf46d7db1a56d70679b13
Omnia-Agabani <omniaagabani99@gmail.com>
2024-12-23 19:05:46 +0400

    Sul_4

--- 3_documenting_and_testing/exercises/mystery_4.py
+++ 3_documenting_and_testing/exercises/mystery_4.py
@@ -1,4 +1,15 @@
 def mystery_4(a):
+    """
+    The function is bring a list count till the given number
+    
+
+    Args:
+        a (int): max number to count
+
+    Returns:
+        list: count till the max number
+    """
+    assert type(a) is int, "a should be an int "
     b = []
 
     c = 0
--- 3_documenting_and_testing/exercises/tests/test_mystery_4.py
+++ 3_documenting_and_testing/exercises/tests/test_mystery_4.py
@@ -8,3 +8,17 @@ class TestMystery4(unittest.TestCase):
     def test_minimal_input(self):
         """"""
         self.assertEqual(mystery_4(0), [])
+    def test_int_input(self):
+        """"""
+        self.assertEqual(mystery_4(5), [0, 1, 2, 3, 4])
+    def test_large_int_input(self):
+        """"""
+        self.assertEqual(mystery_4(15), [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14])
+    def test_str_input(self):
+        """"""
+        with self.assertRaises(AssertionError):
+            mystery_4('A')
+    def test_list_input(self):
+        """"""
+        with self.assertRaises(AssertionError):
+            mystery_4(['a','b'])
