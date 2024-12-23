d14afc0fe8bb27ef4b97d6659e40541114c9797d
Omnia-Agabani <omniaagabani99@gmail.com>
2024-12-23 18:39:34 +0400

    Sul_3

--- 3_documenting_and_testing/exercises/mystery_3.py
+++ 3_documenting_and_testing/exercises/mystery_3.py
@@ -1,4 +1,16 @@
 def mystery_3(a, b):
+    """
+    The function send the minimum value
+    or the summation incase they are equal
+    
+
+    Parameters:
+    2 values from the same type
+    
+    Return:
+    The minimum value or the summation incase they are equal
+    """
+    assert type(a) is type(b), "a and b are not the same type"  
     if a < b:
         return a
     elif a > b:
--- 3_documenting_and_testing/exercises/tests/test_mystery_3.py
+++ 3_documenting_and_testing/exercises/tests/test_mystery_3.py
@@ -13,10 +13,18 @@ class TestMystery3(unittest.TestCase):
     def test_2_int_input(self):
         """"""
         self.assertEqual(mystery_3(17, 23), 17)
+        
+    def test_2_same_int_input(self):
+        """"""
+        self.assertEqual(mystery_3(17, 17), 34)
     
     def test_2_str_input(self):
         """"""
         self.assertEqual(mystery_3('Omnia', 'Agabani'), 'Agabani')
+        
+    def test_2_same_str_input(self):
+        """"""
+        self.assertEqual(mystery_3('Omnia', 'Omnia'), 'OmniaOmnia')
     
     def test_2_list_same_size_input(self):
         """"""
@@ -25,4 +33,10 @@ class TestMystery3(unittest.TestCase):
     def test_2_list_different_size_input(self):
         """"""
         self.assertEqual(mystery_3([3,5], [6,2,5]), [3,5])
-        
+    def test_2_list_same_list_input(self):
+        """"""
+        self.assertEqual(mystery_3([3,5], [3,5]), [3,5,3,5])
+    def test_different_type_input(self):
+        """"""
+        with self.assertRaises(AssertionError):
+            mystery_3('O',['A'])
