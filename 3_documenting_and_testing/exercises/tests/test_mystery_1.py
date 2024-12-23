2f123093d032365af8cf51ed6185ce42347be9c2
Omnia-Agabani <omniaagabani99@gmail.com>
2024-12-23 18:24:44 +0400

    Sul_1

--- 3_documenting_and_testing/exercises/tests/test_mystery_1.py
+++ 3_documenting_and_testing/exercises/tests/test_mystery_1.py
@@ -5,4 +5,25 @@ from ..mystery_1 import mystery_1
 
 class TestMystery1(unittest.TestCase):
     """ """
-    
+    def test_minimal_int_input(self):
+        """"""
+        self.assertEqual(mystery_1(0,0), 0)
+    def test_minimal_str_input(self):
+        """"""
+        self.assertEqual(mystery_1('',''), '')
+    def test_minimal_list_input(self):
+        """"""
+        self.assertEqual(mystery_1([], []), [])
+    def test_2_int_input(self):
+        """"""
+        self.assertEqual(mystery_1(2,5), 7)
+    def test_2_str_input(self):
+        """"""
+        self.assertEqual(mystery_1('O','A'), 'OA')
+    def test_2_list_input(self):
+        """"""
+        self.assertEqual(mystery_1(['O','M'],['F','A']), ['O', 'M', 'F','A'])
+    def test_different_type_input(self):
+        """"""
+        with self.assertRaises(AssertionError):
+            mystery_1('O',['A'])
