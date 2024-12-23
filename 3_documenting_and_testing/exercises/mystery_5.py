902c432f22c974b430ef0c4132c57652c02f902e
Omnia-Agabani <omniaagabani99@gmail.com>
2024-12-23 19:28:20 +0400

    Sul 5

--- 3_documenting_and_testing/exercises/mystery_5.py
+++ 3_documenting_and_testing/exercises/mystery_5.py
@@ -1,4 +1,18 @@
 def mystery_5(a, b):
+    """ 
+    The function sort the givin list
+    and add the other list
+    
+    Parameters:
+    a and b are lists
+    
+    Return:
+    list
+    
+    """
+    assert type(a) is list, "a should be a list "
+    assert type(b) is list, "b should be a list "
+
     while a:
         c = min(a)
         a.remove(c)
--- 3_documenting_and_testing/exercises/tests/test_mystery_5.py
+++ 3_documenting_and_testing/exercises/tests/test_mystery_5.py
@@ -5,7 +5,33 @@ from ..mystery_5 import mystery_5
 class TestMystery5(unittest.TestCase):
     """ """
 
-    def test_minimal_input(self):
+    def test_minimal_list_input(self):
         """"""
         self.assertEqual(mystery_5([], []), [])
+    def test_list_of_int_input(self):
+        """"""
+        self.assertEqual(mystery_5([5,6,98,2,7], []), [2, 5, 6, 7, 98])
+    def test_list_of_str_input(self):
+        """ if b is empty list"""
+        self.assertEqual(mystery_5(['a', 'A', 'Z', 'y'], []), ['A','Z','a','y'])
+    def test_list_of_str2_input(self):
+        """ if b isn't empty list"""
+        self.assertEqual(mystery_5(['a', 'A', 'Z', 'y'], ['z', 5]), ['z',5, 'A','Z','a','y'])
+    def test_int_input(self):
+        """if a is an int"""
+        with self.assertRaises(AssertionError):
+            mystery_5(5, [])
+    def test_str_input(self):
+        """if a is a str"""
+        with self.assertRaises(AssertionError):
+            mystery_5('a', [])
+    def test_int2_input(self):
+        """if b is an int"""
+        with self.assertRaises(AssertionError):
+            mystery_5([], 5)
+    def test_str2_input(self):
+        """if b is a str"""
+        with self.assertRaises(AssertionError):
+            mystery_5([], 'a')
+    
         
