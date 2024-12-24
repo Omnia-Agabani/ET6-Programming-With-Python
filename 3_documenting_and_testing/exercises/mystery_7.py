15141e0a1e4301c6e102723d20ca8aa83af51d93
Omnia-Agabani <omniaagabani99@gmail.com>
2024-12-24 19:55:00 +0400

    sul 7

--- 3_documenting_and_testing/exercises/mystery_7.py
+++ 3_documenting_and_testing/exercises/mystery_7.py
@@ -1,4 +1,13 @@
 def mystery_7(a, b):
+    """This function filters a list by searching for a substring within each element. It takes two parameters:
+
+a: list or string to search through
+b: substring to search for
+
+It returns a new list containing only elements from a that include b as a substring.
+    """
+    assert not isinstance(a , int), "a should be an int"
+    assert not isinstance(b , int), "b should be an int"
     c = []
     for d in a:
         if b in d:
--- 3_documenting_and_testing/exercises/tests/test_mystery_7.py
+++ 3_documenting_and_testing/exercises/tests/test_mystery_7.py
@@ -12,3 +12,15 @@ class TestMystery7(unittest.TestCase):
     def test_minimal_input_string(self):
         """"""
         self.assertEqual(mystery_7('', ''), [])
+    def test_input_list(self):
+        """"""
+        self.assertEqual(mystery_7(['Omnia','Mustafa','Fawzi','Abdulgadir'], 'b'), ['Abdulgadir'])
+
+    def test_input_string(self):
+        """"""
+        self.assertEqual(mystery_7('Omnia', 'a'), ['a'])
+    def test_input_int(self):
+        """"""
+        with self.assertRaises(AssertionError):
+            mystery_7(5, 1)
+    
