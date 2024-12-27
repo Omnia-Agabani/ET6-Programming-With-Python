b70f918f3ee6027463beb14e67b41a23a8bb1154
Omnia-Agabani <omniaagabani99@gmail.com>
2024-12-27 18:49:31 +0400

    sol 9

--- 3_documenting_and_testing/exercises/mystery_9.py
+++ 3_documenting_and_testing/exercises/mystery_9.py
@@ -1,4 +1,14 @@
 def mystery_9(a):
+    """ 
+    The function is sorting a given list
+    
+    parameters:
+    a: list
+    
+    Return:
+    sorted list
+    """
+    assert isinstance(a , list), "a should be a list"
     b = len(a)
     for c in range(b):
         for d in range(0, b - c - 1):
--- 3_documenting_and_testing/exercises/tests/test_mystery_9.py
+++ 3_documenting_and_testing/exercises/tests/test_mystery_9.py
@@ -8,3 +8,23 @@ class TestMystery9(unittest.TestCase):
     def test_minimal_input(self):
         """"""
         self.assertEqual(mystery_9([]), [])
+    def test_list_of_int_input(self):
+        """"""
+        self.assertEqual(mystery_9([1,2,3,4]), [1, 2, 3, 4])
+    def test_list_of_int2_input(self):
+        """"""
+        self.assertEqual(mystery_9([6,2,8,3,15,9]), [2, 3, 6, 8, 9, 15])
+    def test_list_of_str_input(self):
+        """"""
+        self.assertEqual(mystery_9(['om','ni','a']), ['a', 'ni', 'om'])
+    def test_list_of_str2_input(self):
+        """"""
+        self.assertEqual(mystery_9(['om','ni','ag','om','ag']), ['ag', 'ag', 'ni', 'om', 'om'])    
+    def test_string_input(self):
+        """"""
+        with self.assertRaises(AssertionError):
+            mystery_9('omnia')
+    def test_int_input(self):
+        """"""
+        with self.assertRaises(AssertionError):
+            mystery_9(56)
