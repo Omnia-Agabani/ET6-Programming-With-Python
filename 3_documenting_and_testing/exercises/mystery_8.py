2e990c30fc2dd335888b23208bde5b7de0b54e29
Omnia-Agabani <omniaagabani99@gmail.com>
2024-12-27 18:22:13 +0400

    sol 8

--- 3_documenting_and_testing/exercises/mystery_8.py
+++ 3_documenting_and_testing/exercises/mystery_8.py
@@ -1,4 +1,17 @@
 def mystery_8(a, b):
+    """ 
+    The function is giving a list of b in a
+    
+    parameters: 
+    a: is a string or list
+    b is a string
+    
+    Returns:
+    list of b in a
+    """
+    
+    assert not isinstance(a , int), "a should not be an int"
+    assert isinstance(b , str), "b should be a string"
     c = []
     while a:
         if b in a[0]:
--- 3_documenting_and_testing/exercises/tests/test_mystery_8.py
+++ 3_documenting_and_testing/exercises/tests/test_mystery_8.py
@@ -8,3 +8,24 @@ class TestMystery8(unittest.TestCase):
     def test_minimal_input(self):
         """"""
         self.assertEqual(mystery_8([], ''), [])
+    def test_minimal_string_input(self):
+        """"""
+        self.assertEqual(mystery_8('', ''), [])
+    def test_list2_input(self):
+        """a is a list of str"""
+        self.assertEqual(mystery_8(['a', 'g', 'a', 'b', 'a', 'n', 'i'], 'a'), ['a','a','a'])
+    def test_string_input(self):
+        """"""
+        self.assertEqual(mystery_8('omnia agabani', 'a'), ['a','a','a','a'])
+    def test_list3_input(self):
+        """a and b are lists"""
+        with self.assertRaises(AssertionError):
+            mystery_8(['o', 'm', 'n', 'i', 'a'], ['a','g','a','b','a','n', 'i'])
+    def test_list4_input(self):
+        """ b is a list"""
+        with self.assertRaises(AssertionError):
+            mystery_8('o', ['a','g','a','b','a','n', 'i'])  
+    def test_int_input(self):
+        """"""
+        with self.assertRaises(AssertionError):
+            mystery_8(0, 0)
