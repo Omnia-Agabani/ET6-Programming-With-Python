2fdd6155d3863d991a5e19e42090b40680a0fc34
Omnia-Agabani <omniaagabani99@gmail.com>
2024-12-29 17:05:11 +0400

    Draw a triangle

--- Projects_by_Omnia/draw_a_triangle.py
+++ Projects_by_Omnia/draw_a_triangle.py
@@ -8,10 +8,10 @@ def draw_a_triangle (a : int):
   returns:
   draw
   """
+  assert isinstance(a, int), "a must be an integer"
   c = ''
 
   for i in range(a, 0, -1):
-
       c += ('*' * i)+'\n'
 
   return c
--- Projects_by_Omnia/test_draw_a_triangle.py
+++ Projects_by_Omnia/test_draw_a_triangle.py
@@ -6,4 +6,9 @@ class TestAsciiCode(unittest.TestCase):
     self.assertEqual (draw_a_triangle(0),'')
   def test_int(self):
     self.assertEqual (draw_a_triangle(10),'**********\n*********\n********\n*******\n******\n*****\n****\n***\n**\n*\n')
-  
+  def test_error_handling(self):
+        """Test error handling for invalid inputs."""
+        with self.assertRaises(AssertionError):
+            draw_a_triangle('Omnia')  # string
+        with self.assertRaises(AssertionError):
+            draw_a_triangle([1,2,3])  # list
